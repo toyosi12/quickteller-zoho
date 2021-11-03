04c7c8da4439109d71d463115fbe4751d4bf39c5
toyosi12 <toyosioyelayo@gmail.com>
2021-11-03 20:26:45 +0100

    requery and credentials

--- redirect.php
+++ redirect.php
@@ -24,20 +24,46 @@ $retrievedData = explode('&payment_complete_url=', base64_decode(explode('.php/'
 
 $queryString = $retrievedData[0];
 $redirectUrl = $retrievedData[1];
-$payItemId = stringBetween($queryString, '&payment_item_id=', '&payment_mode');
+// $payItemId = stringBetween($queryString, '&payment_item_id=', '&payment_mode');
+$accountId = substr($queryString, strpos($queryString, '&account_id=') + 12);
+$merchantCodePayItemId = explode(':', $accountId);
+$merchantCode = $merchantCodePayItemId[0];
+$payItemId = $merchantCodePayItemId[1];
+$queryString = str_replace('&account_id=' . $accountId, '', $queryString);
 $modQueryString = preg_replace('/&|\?|=/', '', $queryString);
 
 $transactionStatus;
+$transactionReference = stringBetween($queryString, '&gateway_reference_id=', '&payment_mode=card');
+$transactionReference = str_replace('gateway_reference_id=', '', $transactionReference);
+
+$amount = stringBetween($queryString, '?amount=', '&gateway_reference_id=');
+$amount = str_replace('amount=', '', $amount) * 100;
 
 if ($_POST['resp'] == '00') {
-    $transactionStatus = 1;
+    //confirm transaction
+    $requeryUrl = 'https://qa.interswitchng.com/collections/api/v1/gettransaction.json?merchantcode='
+    . $merchantCode . '&transactionreference=' . $transactionReference . '&amount=' . $amount;
+    
+    $curlInit = curl_init();
+    curl_setopt($curlInit, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json'
+    ));
+    curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curlInit, CURLOPT_URL, $requeryUrl);
+    $response = json_decode(curl_exec($curlInit));
+    curl_close($curlInit);
+    if ($response->ResponseCode == '00') {
+        $transactionStatus = 1;
+    } else {
+        $transactionStatus = 0;
+    }
 } else {
     $transactionStatus = 0;
 }
 $modQueryString .= 'transaction_status' . $transactionStatus;
 
 
-$signature = hash_hmac('sha256', $modQueryString, 'Default_Payable_MX26070');
+$signature = hash_hmac('sha256', $modQueryString, $payItemId);
 $queryString .= '&transaction_status=' . $transactionStatus . '&signature=' . $signature;
 
 header('Location: ' . $redirectUrl . $queryString);
